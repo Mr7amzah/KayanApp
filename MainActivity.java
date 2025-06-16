b77f6f332fe25c2c330773e706051b6f65d8484f
Mr7amzah <hamzahwah@gmail.com>
2025-06-16 03:25:46 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,28 @@
 package com.kayanapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button salesButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        salesButton = findViewById(R.id.btnSales);
+        salesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SalesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
