bd91f8994a325a7cee9ecc2b9b47d109ed68c7e1
Mr7amzah <hamzahwah@gmail.com>
2025-06-16 03:34:39 +0300

    Update SalesActivity.java

--- app/app/src/main/res/layout/app/src/main/java/com/kayan/app/src/main/java/com/kayan/app/src/main/res/layout/app/src/main/java/com/kayan/SalesActivity.java
+++ app/app/src/main/res/layout/app/src/main/java/com/kayan/app/src/main/java/com/kayan/app/src/main/res/layout/app/src/main/java/com/kayan/SalesActivity.java
@@ -1,14 +1,17 @@
-package com.kayan;
+package com.kayanapp;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class SalesActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales);
-        setTitle("المبيعات");
+
+        TextView text = findViewById(R.id.textSales);
+        text.setText("مرحباً بك في قسم المبيعات");
     }
 }
